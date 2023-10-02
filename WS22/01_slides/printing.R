d63e82b82d80ee3aa97ef20c7e10aa5179364563
jens-klenke <57100601+jens-klenke@users.noreply.github.com>
2023-10-02 12:00:37 +0200

    pdf versions

--- WS22/01_slides/printing.R
+++ WS22/01_slides/printing.R
@@ -2,12 +2,10 @@
 # needs chrome
 
 # 1
-pagedown::chrome_print(here::here("SoSe22/01_slides/vorkurs_1.Rmd"))
+pagedown::chrome_print(here::here("WS23/01_slides/vorkurs_1.Rmd"))
 
 # 2
-pagedown::chrome_print(here::here("SoSe22/01_slides/vorkurs_2.Rmd"))
+pagedown::chrome_print(here::here("WS23/01_slides/vorkurs_2.Rmd"))
 
 # 3
-pagedown::chrome_print(here::here("SoSe22/01_slides/vorkurs_3.Rmd"))
-
-
+pagedown::chrome_print(here::here("WS23/01_slides/vorkurs_3.Rmd"))
